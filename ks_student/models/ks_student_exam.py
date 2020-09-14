7b4109fe728070ce247246769ae51149baad0d6e
sanjeetaksolves <sanjeetaksolves@ksolves.com>
2020-09-14 22:38:54 +0530

    1.Added 'ks_student_class_id' in ks_class_subject model. 2.Added 'ks_exam_id_domain' function. 3.Update view of ks_class_subject model.

--- ks_student/models/ks_student_exam.py
+++ ks_student/models/ks_student_exam.py
@@ -6,10 +6,15 @@ class KsStudentExam(models.Model):
     _name = 'ks.student.exam'
     _description = 'Student Exam'
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char('Exam Name', required=True)
     ks_student_id = fields.Many2one('ks.student', 'Student ID', required=True)
-    ks_exam_id = fields.Many2one('ks.class.subject', 'Exam ID', required=True,
-                                 domain="[('ks_student_id', '=', ks_student_id)]")
     ks_student_class_id = fields.Many2one('ks.student.class', 'Student Class ID', required=True)
+    ks_exam_id = fields.Many2one('ks.class.subject', 'Exam ID', required=True)
+
     ks_obtained_marks = fields.Integer('Obtained Marks', required=True)
+
+    @api.onchange('ks_student_class_id')
+    def ks_exam_id_domain(self):
+        for rec in self:
+            return {'domain':  {'ks_exam_id': [('ks_student_class_id', '=', rec.ks_student_class_id.id)]}}
 
